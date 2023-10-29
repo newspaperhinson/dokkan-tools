f21d7e10672c76006801c1c9be2d1c5fb363fa8d
Hinson <newspaperhinson@gmail.com>
2023-10-29 14:57:15 +0800

    implemented server action for entering app

---
+++ src/actions/enterApp.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import prisma from "@/services/prisma";
+import { redirect } from "next/navigation";
+
+export default async function enterApp(formData: FormData): Promise<void> {
+  const username = formData.get("username") as string;
+
+  // find existing user in database
+  const user = await prisma.user.findUnique({
+    where: {
+      username: username
+    }
+  });
+
+  // create new user if no existing user
+  if (!user) {
+    await prisma.user.create({
+      data: {
+        username
+      }
+    });
+  }
+
+  // redirect to ultimate clash page
+  redirect("/ultimate-clash");
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
+import enterApp from "@/actions/enterApp"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <form action={enterApp}>
+        <span>Enter dokkan-tools as</span>
+        <input
+          className="border border-neutral-700 mx-4"
+          type="text"
+          name="username"
+          id="username"
+        />
+        <input
+          className="border border-neutral-700 p-1 cursor-pointer"
+          type="submit"
+          value="enter"
+        />
+      </form>
     </main>
   )
 }
