899e5d419f9b151aff0b1b38c52cb9e9ab2a5985
Hinson <newspaperhinson@gmail.com>
2023-11-07 00:04:22 +0800

    updated prisma schema

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
 class PrismaClientSingleton {
-  private static instance: PrismaClient | undefined;
+  private static instance?: PrismaClient;
 
   public static getInstance() {
     if (PrismaClientSingleton.instance === undefined) {
       PrismaClientSingleton.instance = new PrismaClient();
     }
-    return PrismaClientSingleton.instance
+    return PrismaClientSingleton.instance;
   }
 }
 
-export default PrismaClientSingleton.getInstance()
+export default PrismaClientSingleton.getInstance();
