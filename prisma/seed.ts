591c82ad046fa97b4951de680b305b14446fb641
Hinson <newspaperhinson@gmail.com>
2023-11-25 18:44:33 +0800

    schema and seeding for ultimate clash

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,6 +15,28 @@ async function main() {
       type: "AGL"
     }
   });
+
+  const jacoUC = await  prisma.ultimateClashEnemy.upsert({
+    where: { session_level_stage: { session: 68, level: 1, stage: 1 } },
+    update: {},
+    create: {
+      ultimateClash: {
+        create: {
+          session: 68,
+          start: new Date("2023-10-31T16:00:00"),
+          end: new Date("2023-11-24T15:59:59"),
+        },
+      },
+      level: 1,
+      stage: 1,
+      card: {
+        connect: { id: 1009120 },
+      },
+      rarity: "UR",
+      class: "SUPER",
+      type: "AGL", 
+    },
+  })
 }
 
 main()
