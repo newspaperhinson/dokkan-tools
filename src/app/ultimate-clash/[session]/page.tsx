a76bb257bc7e779f98329f84883106e652f1e814
Hinson <newspaperhinson@gmail.com>
2023-12-03 20:35:18 +0800

    getting UC enemies

---
+++ src/app/ultimate-clash/[session]/page.tsx
@@ -0,0 +1,37 @@
+import prisma from "@/libs/prisma";
+
+// generate params for all sessions
+export async function generateStaticParams() {
+  const ultimateClashes = await prisma.ultimateClash.findMany({});
+
+  return ultimateClashes.map((ultimateClash) => (ultimateClash.session));
+};
+
+export default async function UltimateClashPage({ params }: { params: { session: string }}) {
+  // get enemies of that session
+  const enemies = await prisma.ultimateClashEnemy.findMany({
+    where: { session: parseInt(params.session, 10) },
+    include: {
+      card: {
+        include: {
+          character: true,
+        }
+      },
+    },
+    orderBy: [
+      { level: "asc" },
+      { stage: "asc" },
+    ],
+  });
+
+  return (
+    <div>
+      {enemies.map((enemy, index) => (
+        <div key={index}>
+          <span>{enemy.cardId}</span>
+          <span>{enemy.card.character.name}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
